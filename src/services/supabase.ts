2fa4f7eaf3d20cf0878f5f5824ff3bcd74719a0e
SuprajaAle <67367324+SuprajaAle@users.noreply.github.com>
2025-09-15 22:06:18 +0530

    Updated supabase.ts

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://hczgfrgrpfvardehglfy.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjemdmcmdycGZ2YXJkZWhnbGZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NjIwOTcsImV4cCI6MjA3MzQzODA5N30.ly5jtBFfqG0mTq7kfWJ0K8gdqMkUhHt9jGQUOqODbwU"
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables');
